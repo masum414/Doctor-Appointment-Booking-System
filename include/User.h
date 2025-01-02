73a4a0de39430599fee0db73d5af51f7fe07c74d
Jannatul Ferdus Kona <islamdua2000@gmail.com>
2025-01-02 23:12:30 +0600

    User.h file added

--- include/User.h
+++ include/User.h
@@ -2,31 +2,23 @@
 #define USER_H
 
 #include <string>
-#include "DataValidator.h"
 
 using namespace std;
 
 class User
 {
 public:
+    static int nextID;
     int id;
     string name;
     string phone;
     string email;
     string password;
 
-    // To keep track of the next ID
-    static int nextId;
-
-public:
     User(const string &name, const string &phone, const string &email, const string &password);
 
-    // Pure virtual functions for login and registration
-    virtual void loginUser() = 0;
-    virtual void registerUser() = 0;
-
-    // To update user details
-    void updateDetails();
+    // Method to update user details
+    virtual void updateDetails();
 };
 
 #endif
