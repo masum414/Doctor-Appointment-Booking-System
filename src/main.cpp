44774e02a288a6658c940b46d750e92021df128e
Mahbub Alam Masum <mail.m45um@gmail.com>
2025-01-03 20:55:00 +0600

    main.cpp implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,31 @@
 #include <iostream>
-#include <json.hpp>
-#include <fstream>
+#include <vector>
+#include "Patient.h"
+#include "JsonHandler.h"
+#include "CLI.h"
 
-using json = nlohmann::json;
 using namespace std;
 
 int main()
 {
-    // Create a JSON object
-    json j;
-    j["name"] = "Masum";
-    j["age"] = 21;
+    // Vectors to hold patients and doctors
+    vector<Patient> patients;
+    vector<json> doctors;
 
-    // Output JSON object to the console
-    cout << j << endl;
+    // Create an instance of JsonHandler to manage file operations
+    JsonHandler jsonHandler;
 
-    // Write the JSON object to a file
-    string filePath = "data/doctors.json";
-    ofstream outFile(filePath);
-    if (outFile.is_open())
-    {
-        outFile << j.dump(4);
-        outFile.close();
-        cout << "JSON data has been written to " << filePath << endl;
-    }
-    else
-    {
-        cout << "Failed to open file " << filePath << endl;
-    }
+    // Load data from JSON files into vectors
+    jsonHandler.loadDoctors(doctors);
+    jsonHandler.loadPatients(patients);
 
-    // Read the JSON data back from the file
-    ifstream inFile(filePath);
-    if (inFile.is_open())
-    {
-        json j2;
-        inFile >> j2;
-        inFile.close();
+    // Create an instance of CLI for user interaction
+    CLI cli(patients, doctors, jsonHandler);
+    cli.displayMainMenu();
 
-        cout << "Read JSON data from file:" << endl;
-        cout << j2.dump(4) << endl;
-
-        cout << "Name: " << j2["name"] << ", Age: " << j2["age"] << endl;
-    }
-    else
-    {
-        cout << "Failed to open file " << filePath << endl;
-    }
+    // Save data back to JSON files before exiting
+    jsonHandler.saveDoctors(doctors);
+    jsonHandler.savePatients(patients);
 
     return 0;
 }
